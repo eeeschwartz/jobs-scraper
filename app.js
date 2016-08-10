4119bc8fa17397f9a6ca2898c3c478e09e0678be
Erik Schwartz <erik@erikschwartz.net>
2016-08-10 16:00:33 -0400

    Add job permalink

--- app.js
+++ app.js
@@ -17,10 +17,15 @@ function getJobs() {
   var $ = cheerio.load(fs.readFileSync('jobs.html'));
 
   $('.PSLEVEL1GRID tr').each(function(x, el) {
+    // skip headers
+    if ($(el).find('th').length > 0) { return; }
+
+    var td = $(el).find('td');
     var item = {
-        title: $(el).find('.PSHYPERLINK a').html(),
-        date: $(el).find('.PSEDITBOX_DISPONLY').html(),
-        url: 'http://letsencrypt.erikschwartz.net/'
+        //title: $(el).find('.PSHYPERLINK a').html(),
+        date: td.next().find('span').html(),
+        title: td.next().next().find('a.PSHYPERLINK').html(),
+        url: 'https://jobs.lexingtonky.gov/psp/candidate/EMPLOYEE/HRMS/c/HRS_HRAM.HRS_CE.GBL?Page=HRS_CE_JOB_DTL&Action=A&SiteId=1&PostingSeq=1&JobOpeningId=' + td.next().next().next().find('span').html()
     };
     item.guid = '' + item.title + item.date;
     item.guid = crypto.createHash('md5').update(item.guid).digest('hex');
